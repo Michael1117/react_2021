83131be7a8871a2416725d2f3ea37942cff0c933
hee <Michael1117@users.noreply.github.com>
2021-02-13 21:55:04 +0800

    属性校验

--- src/index.js
+++ src/index.js
@@ -1,55 +1,98 @@
 import React from "react"; // 核心库
 import ReactDOM from "react-dom"; // DOM渲染库
+import PropTypes from "prop-types";
 
 /**
- *  如何定义组件 和组件的属性
- *  函数式组件  就是一个函数  接收一个属性对象 返回一个react元素
- *  类组件  就是一个类，需要有一个render方法， render方法需要返回一个并且仅能返回一个顶级React元素
+ *  如何 对属性进行类型校验
+ *    组件封装好是给别人用的
  */
 
-/**
- *  如何渲染函数组件？
- *  1. 封装函数组件的属性对象 props = {name: 'title'}
- *  2. 把props传递给Welcome1这个函数，返回一个React元素
- *  3. 把这个React元素，也就是虚拟DOM渲染到真实DOM上
- */
-function Welcome1(props) {
-  return <h1>hello {props.name}</h1>;
-}
-
-/**
- *  如何渲染函数组件？
- *  1. 封装函数组件的属性对象 props = {name: 'title'}
- *  2. new Welcome2(props); 创建Welcome2这个类的实例，传递props进去  this.props
- *  3. 调用实例的render方法， 得到返回的react元素
- *  4. 把这个React元素， 也就是虚拟DOM渲染到真实DOM上
- */
-
-class Welcome2 extends React.Component {
-  constructor(props) {
-    debugger;
-    super(props);
-
-    //this.props = props;
-  }
+class Person extends React.Component {
+  static defaultProps = {
+    name: "张三",
+  };
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    gender: PropTypes.oneOf(["male", "female"]),
+    hobby: PropTypes.arrayOf(PropTypes.string), // 字符串数组
+    position: PropTypes.shape({
+      // position的值是一个对象 key  x  y 值是number
+      x: PropTypes.number,
+      y: PropTypes.number,
+    }),
+    // 属性对象  属性名称  组件名称  age是设置了一个函数，    自定义的校验器
+    age(props, propName, componentName) {
+      let age = props[propName];
+      if (age < 0 || age > 120) {
+        throw new Error(
+          `Invalid prop ${propName} of value ${age} supplied to ${componentName}, expected age between 0 and 120`
+        );
+      }
+    },
+  };
   render() {
-    return <h1>hello {this.props.name}</h1>;
+    let { name, age, gender, hobby, position, friends } = this.props;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <td>姓名</td>
+            <td>年龄</td>
+            <td>性别</td>
+            <td>爱好</td>
+            <td>位置</td>
+            <td>朋友</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{name}</td>
+            <td>{age}</td>
+            <td>{gender}</td>
+            <td>{hobby.join(",")}</td>
+            <td>{`x:${position.x}, y: ${position.y}`}</td>
+            <td>{friends[0].name}</td>
+          </tr>
+        </tbody>
+      </table>
+    );
   }
 }
 
-/**
- * 1. 类组件   函数组件和类组件首字母必须大写
- * React 是通过首字母来区分到底是自定义组件(大写)你还是原生DOM组件(小写)
- * 2. 组件必须在使用的时候先定义
- * 3. 必须只能返回一个顶级元素
- * 4. props
- */
+// 先创建一个props对象
+let props = {
+  //name: 'Michael',
+  age: 80,
+  gender: "male",
+  hobby: ["smoking", "drinking"],
+  position: {
+    x: 10,
+    y: 10,
+  },
+  friends: [
+    { name: "张三", age: 10 },
+    { name: "李四", age: 12 },
+  ],
+};
 
- /**
-  * 1. props具有只读性
-  * 2. 不管以何种方式声明组件， 都不能修改自己的props
-  *  纯函数  1. 不能改变输入的值， 2. 如果输入的值相当， 输出结果也一定相同
-  */
-let element = <Welcome2 name="title"></Welcome2>;
+let element = <Person {...props} />;
+// 1. 拿到 props
+let componentInstance = new Person(props);
+let propTypes = Person.propTypes;
+if (propTypes.name === PropTypes.string.isRequired) {
+  if (!props.name) {
+    throw new Error('属性名name必须提供')
+  }
+}
 
 ReactDOM.render(element, document.getElementById("root"));
+
+/**
+ * 为什么  类型检查是静态属性啊
+ * 什么时候动态  什么时候静态
+ *    有些需要动态和静态都可以，那应该使用哪个？
+ *      如果能使用类的静态属性实现，就不要用实例属性
+ *      1. 使用方便  Person.propTypes
+ *      2.  只会有一份， 节约内存  实例属性，每new 一个实例  都要创建一份。但是静态永远只有一份
+ *          能共用就共用，节约资源，不能共用才需要每个一份
+ */
