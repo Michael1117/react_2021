e952de813cd08c7bf6da592a28f34dbf0cc38c42
hee <Michael1117@users.noreply.github.com>
2021-02-13 22:35:40 +0800

    封装前

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
-import React from "react"; // 核心库
-import ReactDOM from "react-dom"; // DOM渲染库
-import PropTypes from "prop-types";
+import React from "./react"; // 核心库
+import ReactDOM from "./react-dom"; // DOM渲染库
 
-/* let element = (
-  <h1 className="title" style={{ color: 'red'}}>
-    hello
-    <span>world</span>
-  </h1>
-)
- */
 
-let element = React.createElement(
-  "h1",
-  {
-    className: "title",
-    style: {
-      color: "red",
-    },
-  },
-  "hello",
-  React.createElement("span", null, "world")
-);
+let element = React.createElement("h1", {
+  className: 'title',
+  style: {
+    color: 'red'
+  }
+}, "hello", React.createElement("span", null, "world"))
 
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(element, document.getElementById('root'))
--- src/react-dom.js
+++ src/react-dom.js
@@ -1,5 +1,25 @@
 function render(element, container) {
+    if (typeof element === 'string' || typeof element === 'number') {
+        return container.appendChild(document.createTextNode(element))
+    }
+    let {type, props} = element;
+    let dom = document.createElement(type)
+    for (let propName in props){    // 循环每一个特性
+        if (propName === 'children') {
+            props.children.forEach(child => render(child, dom))
+        } else if(propName === 'className') {
+            dom.className = props[propName];
+        } else if (propName === 'style'){
+            let styleObj = props[propName];
+            for (let attr in styleObj) {
+                dom.style[attr] = styleObj[attr]
+            }
+        } else {
+            dom.setAttribute(propName, props[propName])
+        }
+    }
 
+    container.appendChild(dom)
 }
 
 
