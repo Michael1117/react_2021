744f0b13ec77123d880712d731ac0c103af5f6d4
hee <Michael1117@users.noreply.github.com>
2021-02-13 18:43:56 +0800

    map

--- src/index.js
+++ src/index.js
@@ -1,43 +1,20 @@
 import React from 'react';   // 核心库
 import ReactDOM from 'react-dom'; // DOM渲染库
 
-/**
- * 
- * 把h1元素渲染到root节点的内部
- * 看起来像html,但是其实是JS, 它并不是标准的JS语法
- * 在webpack打包的时候，会把这种JSX语法转成纯的JS代码， 所以在浏览器就可以运行了
- */
+// 当你map的时候，是把一个字符串数组映射为了一个li的React元素数组
+// 在列表中的每一个子元素都应该有一个唯一的key属性
+// 原因是为了高效的dom  diff
+let names = ['zhangsan', 'lisi', 'wangwu']
+let elements = names.map((item, index) => <li key={item}>{item}</li>)
 
- /**
-  *  什么叫React元素
-  *   是React应用的最小单位，它描述了你在屏幕上看到的内容
-  *   React元素的本质是普通的 JS 对象
-  *   ReactDOM会保证浏览器中的DOM和你的React元素一致
-  */
-
-//  let element = <h1 id="title">hello<h2>world</h2></h1>
-/* ReactDOM.render(
-  <h1>hello</h1>,
+ReactDOM.render(
+  <ul>{elements}</ul>,
   document.getElementById('root')
-);
- */
+)
 
-/* function createElement(type, config = {}, ...children) {
-  return {
-    //$$typeof: Symbol.for('react.element'),
-    type,
-    props: {...config, children}
-  }
-}
+let elements2 = names.reverse().map((item, index) => <li key={item}>{item}</li>)
 
- let element = createElement('h1', {id: 'title'}, 'hello', createElement('h2', null, 'world'))
-
- console.log(element) */
-
- let name = 'Michael'
- let element = <h1 id="title">hello {name}</h1>
-
- ReactDOM.render(
-   element,
-   document.getElementById('root')
- )
+ReactDOM.render(
+  <ul>{elements2}</ul>,
+  document.getElementById('root')
+)
