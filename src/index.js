e4d05c321c4bdd6915e0a1fd330bf9ec236cbb68
hee <Michael1117@users.noreply.github.com>
2021-02-17 22:17:54 +0800

    setState

--- src/index.js
+++ src/index.js
@@ -8,12 +8,22 @@ import ReactDom from "react-dom";
  * 属性和状态都可以影响视图，他们改变了都会引起视图的更新
  */
 
+ /**
+  * 1. 不要直接修改state  直接修改不能刷新页面 setState包含了刷新界面的操作， 就是让真实DOM和最新虚拟DOM保持 一致
+  * 2. setState 的更新可能会被合并  说明setState里传的对象会跟老的对象进行合并， 并不会直接  覆盖
+  * 
+  *   老状态中有新状态中也有  则更新
+  *   老状态中没有新状态中有  则添加
+  *   老状态中有新状态没有则保持原值
+  * 3. setState 的更新可能是异步的
+  *   在事件处理函数里setState的时候并不会直接修改状态，而是先把partialState放入一个数组(链表中)缓存起来， 等事件执行结束了，再统一进行更新
+  */
+
 class Clock extends React.Component {
-  state = { date: new Date() }  // 两种定义初始状态的方式
+  // state = { date: new Date() }  // 两种定义初始状态的方式
   constructor(props) {
     super(props); // this.props = props;
-    //this.state = { date: new Date() };
-    // 其它地方要改变只能调用setState
+    this.state = { number: 0,  name: '计数器' }
   }
   componentWillMount() {
     // 组件将要挂载
@@ -25,13 +35,41 @@ class Clock extends React.Component {
       this.setState({ date: new Date() });
     }, 1000);
   }
+  /**
+   * let updateQueue = []
+   * updateQueue.push({number: this.state.number + 1})
+   */
+  handleClick = () => {
+    /* this.setState({ number: this.state.number + 1 , age: 18})
+    console.log(this.state.number)
+    this.setState({ number: this.state.number + 1})
+    console.log(this.state.number) */
+    /* this.setState({ number: this.state.number + 1 } , () => {
+      console.log(this.state.number)
+    }) */
+
+    this.setState(prevState => ({number: prevState.number + 1 }), () => {
+      console.log('3', this.state.number)
+    })
+    console.log('1', this.state.number)
+    this.setState(prevState => ({number: prevState.number + 1 }), () => {
+      console.log('4', this.state.number)
+    })
+    console.log('2', this.state.number)
+  }
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
-        <h2>当前的时间为： {this.state.date.toLocaleTimeString()}</h2>
+        <h1>{this.state.number} {this.state.name} {this.state.age} </h1>
+        <button onClick={this.handleClick}>+</button>
       </div>
     );
   }
 }
 ReactDom.render(<Clock />, document.getElementById("root"));
+
+/**
+ * 绑定事件的时候  虚拟DOM  JSX绑定和原生DOM不一样
+ * 1. 属性名  JSX  onClick   原生DOM  onclick
+ * 2.  值  JSX 函数的指针  函数变量名  原生  字符串
+ */
